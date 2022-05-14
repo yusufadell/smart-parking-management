65356720b4a730a95a695091901ea6af6fc96a4c
yusufadell <youseefadel777@gmail.com>
2022-05-15 01:41:48 +0200

    Automatic configuration with django-heroku

--- parking_management/settings.py
+++ parking_management/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
 import os
 
@@ -143,3 +142,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+import django_heroku
+django_heroku.settings(locals())
